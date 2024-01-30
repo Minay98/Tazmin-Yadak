5aea12c5db2e87cdfbd51560f39e7fa21e38d312
Mina Yousefnezhad <93423309+Minay98@users.noreply.github.com>
2024-01-30 18:25:04 +0330

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./build/*.html'],
+  theme: {
+    extend: {
+      screens: {
+   
+  
+        'xl': '1440px',
+
+      }
+    },
+  },
+  plugins: [],
+}
+
